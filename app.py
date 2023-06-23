7768bc7f31b512de84ed43c362262f8c6064fee8
tarik37 <tarik.medjahed@gmail.com>
2023-06-23 15:18:49 +0200

    update app

--- app.py
+++ app.py
@@ -1,14 +1,21 @@
 import pandas as pd
+import requests
 from flask import Flask, render_template, request
 from flask_bootstrap import Bootstrap
 
 from sqlalchemy import create_engine
+from sqlalchemy.orm import sessionmaker
 from azure.identity import DefaultAzureCredential
 from azure.appconfiguration import AzureAppConfigurationClient
 from forms import AddressForm
+import json
 
-import requests
+from utils import haversine_distance
+
 import os
+from glob import glob
+
+import joblib
 
 app = Flask(__name__)
 
@@ -53,18 +60,75 @@ def analyse():
 
 @app.route('/model', methods=['GET', 'POST'])
 def model():
-    form = AddressForm()
-    suggestions = []  # Initialize the suggestions variable as an empty list
+    # Create the AzureAppConfiguration client
+    credential = DefaultAzureCredential()
+    appconfig_conn_str = "Endpoint=https://app-co2-config.azconfig.io;Id=8/Iv;Secret=8qfVLXI2aDt1Wg0jPMnCLx5lHIDvdAuzucBti8of7+M="
+    appconfig_client = AzureAppConfigurationClient.from_connection_string(connection_string=appconfig_conn_str)
+    
+    # Retrieve the connection string from Azure App Configuration
+    setting = appconfig_client.get_configuration_setting(key="pg-connect-string")
+    secret_value = setting.value
+    # Retrieve the Bing key API for geocoding from Azure App Configuration
+    setting_bing = appconfig_client.get_configuration_setting(key="BING-API-KEY")
+    bingApiKey = setting_bing.value
+     
+    # Create the SQLAlchemy engine
+    engine = create_engine(secret_value)
+    print(engine)
+    
+        
+    suggestions = []
     coordinates = []
+    latitude = None
+    longitude = None
+    
+    # Get the model names from data file to populate the dropdown box
+    model_files = glob('data/best_model_*.pkl')
+    model_names = [os.path.basename(file)[:-4] for file in model_files]
+    # Query the distinct building types
+    query2 = "SELECT DISTINCT buildingtype FROM super_table_tm WHERE datayear = 2016"
+    building_types = pd.read_sql_query(query2, engine)["buildingtype"].tolist()
 
-    if form.validate_on_submit():
-        address = form.address.data
+    # Query the distinct primary property types
+    query3 = "SELECT DISTINCT primarypropertytype FROM super_table_tm WHERE datayear = 2016"
+    property_types = pd.read_sql_query(query3, engine)["primarypropertytype"].tolist()
 
+    if request.method == 'POST':
+        # adress field
+        address = request.form['address']
+        
+        # yearbuilt field
+        yearbuilt = request.form['yearbuilt']
+        
+        # buildingtype field
+        buildingtype = request.form['buildingtype']
+        
+        # primarypropertytype field
+        primarypropertytype = request.form['primarypropertytype']
+        
+        # primarypropertytype field
+        largestpropertyusetypegfa = request.form['largestpropertyusetypegfa']
+        
+        # propertygfabuildings field
+        propertygfabuildings = request.form['propertygfabuildings']
+        
+        # numberofbuildings field
+        numberofbuildings = request.form['numberofbuildings']
+        
+        # numberoffloors field
+        numberoffloors = request.form.get('numberoffloors')
+
+        is_using_naturalgaskWh = request.form.get('is_using_naturalgaskWh', '0')
+        
+        is_using_steamusekWh = request.form.get('is_using_steamusekWh', '0')
+        
+        is_using_electricitykWh = request.form.get('is_using_electricitykWh', '0')
+        
         # Bing API request for auto-completion
         suggestion_url = 'http://dev.virtualearth.net/REST/v1/Autosuggest'
         suggestion_params = {
             'query': address,
-            'key': 'AqCmXCSXpin76Mn5hLW5CVVvqbY88Ae9TbqET5mzzwohbCvs-ZbboA-OWuAoNThc'
+            'key': bingApiKey
         }
         suggestion_response = requests.get(suggestion_url, params=suggestion_params)
         suggestions = suggestion_response.json().get('resourceSets')[0].get('resources')
@@ -73,15 +137,54 @@ def model():
         geocode_url = 'http://dev.virtualearth.net/REST/v1/Locations'
         geocode_params = {
             'q': address,
-            'key': 'AqCmXCSXpin76Mn5hLW5CVVvqbY88Ae9TbqET5mzzwohbCvs-ZbboA-OWuAoNThc'
+            'key': bingApiKey
         }
         geocode_response = requests.get(geocode_url, params=geocode_params)
-        coordinates = geocode_response.json().get('resourceSets')[0].get('resources')[0].get('point').get('coordinates')
 
-        return render_template('model.html', form=form, suggestions=suggestions, coordinates=coordinates)
+        try:
+            geocode_data = geocode_response.json()
+            coordinates = geocode_data.get('resourceSets')[0].get('resources')[0].get('point').get('coordinates')
+            latitude = coordinates[0]
+            longitude = coordinates[1]
+            # Center of Seattle coordinates
+            seattle_center_lat = 47.6062
+            seattle_center_lon = -122.3321
 
-    return render_template('model.html', form=form, suggestions=suggestions, coordinates=coordinates)
+            # Calculate Haversine distance and angle for the request adress in the formular
+            haversinedistance = haversine_distance(seattle_center_lat, seattle_center_lon, latitude, longitude)
+            # Create the table of df_prediction
+            df_input = pd.DataFrame({
+                'haversinedistance': [haversinedistance],  # Wrap scalar value in a list
+                'yearbuilt': [yearbuilt],
+                'is_using_electricitykWh': [is_using_electricitykWh],
+                'is_using_naturalgaskWh': [is_using_naturalgaskWh],
+                'is_using_steamusekWh': [is_using_steamusekWh],
+                'largestpropertyusetypegfa': [largestpropertyusetypegfa],
+                'numberofbuildings': [numberofbuildings],
+                'numberoffloors': [numberoffloors],
+                'propertygfabuildings': [propertygfabuildings],
+                'buildingtype': [buildingtype],
+                'primarypropertytype': [primarypropertytype]
+            }, index=[0])  # Add index sinon ValueError: If using all scalar values, you must pass an index
+            
+            # get selected model selected_model field
+            selected_model = request.form['model_name']
+            # import model avec joblib
+            loaded_model = joblib.load(f'data/{selected_model}.pkl')
+            # prediction avec le modele
+            y_pred = loaded_model.predict(df_input)
+            
+        except json.decoder.JSONDecodeError as e:
+            print('Error decoding JSON response:', e)
+            print('Response content:', geocode_response.content)
+        
+                
+        return render_template('model.html', building_types=building_types, property_types=property_types, suggestions=suggestions, coordinates=coordinates, latitude=latitude,
+                               longitude=longitude, bing_maps_api_key=bingApiKey,table = df_input.to_html(),
+                               model_names=model_names, prediction=y_pred)
+    return render_template('model.html', building_types=building_types, property_types=property_types, suggestions=suggestions, coordinates=coordinates, latitude=latitude,
+                           longitude=longitude, bing_maps_api_key=bingApiKey,model_names=model_names)
 
 if __name__ == '__main__':
-    app.run(host="0.0.0.0", port=5000)
+    app.run(host="0.0.0.0", port=5000, debug=True)
 
